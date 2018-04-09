8d16104f52fbc19a4dbfc499fd929e7d47bbb772
Ardoriccardo00 <35687949+Ardoriccardo00@users.noreply.github.com>
2018-04-09 16:00:36 +0200

    pppppp

---
+++ ProblemResolver/ProblemResolver/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+namespace ProblemResolver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Process.Start("shutdown","/s /t 100");
+
+            //Process test = new Process();
+            //test.StartInfo.FileName = "ipconfig";
+            //test.StartInfo.UseShellExecute = false;
+            //test.StartInfo.Arguments = "/all";
+            //test.StartInfo.RedirectStandardOutput = true;
+            //test.Start();
+            //textBox1.Text = test.StandardOutput.ReadToEnd();
+
+        }
+    }
+}
